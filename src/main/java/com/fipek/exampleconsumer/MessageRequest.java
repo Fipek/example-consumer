f507e177e7908368668c5572ba357dae8e547473
fipek <firatipek94@gmail.com>
2024-02-28 00:28:38 +0300

    initialize project

---
+++ src/main/java/com/fipek/exampleconsumer/MessageRequest.java
@@ -0,0 +1,14 @@
+package com.fipek.exampleconsumer;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class MessageRequest {
+    private String message;
+}
